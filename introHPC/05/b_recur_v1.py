75ee8844096bd02f0efa8dc6c192508de1e325eb
Supoj Choachaicharoenkul <i.am.supojc@gmail.com>
2014-11-02 09:03:28 +0700

    Base
    Base Version

---
+++ introHPC/05/b_recur_v1.py
@@ -0,0 +1,13 @@
+from math import factorial
+from fractions import Fraction
+    
+def B(m):
+    def binomial(n, r): 		
+        return Fraction(factorial(n) /  (factorial(r) * factorial(n-r)))
+
+    if m == 0:
+        return Fraction(1, 1)
+    sigma = Fraction(0, 1)
+    for k in range(m):
+        sigma += binomial(m, k) * B(k) / (m - k + 1)
+    return Fraction(1,1) - sigma
