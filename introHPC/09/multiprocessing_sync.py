28e5270bc147154328012fe1ad169d8ca21ad2ac
hpc-thai <i.am.supojc@gmail.com>
2014-12-15 13:05:18 +0700

    race2

--- introHPC/09/multiprocessing_sync.py
+++ introHPC/09/multiprocessing_sync.py
@@ -2,24 +2,24 @@ import multiprocessing as mp
 
 MAX = 10000
 nthreads = 2
-
-
-def worker(d, threadNo, sumWorkers):
+  
+def worker(lock1, d, threadNo, sumWorkers):
     sumLocal = 0
     while True:
-        if  d.empty() == True: 
-            break
-        sumLocal += d.get(block=False)
+        with lock1:
+            if  d.empty() == True: 
+                break
+            sumLocal += d.get(block=False)
     sumWorkers.put(sumLocal)
     print("Thread[%d]: %d" %(threadNo, sumLocal))
- 
-   
+    
 if __name__ == '__main__':
     d = mp.Queue()
+    lock1 = mp.Lock()
     sumWorkers = mp.Queue()
     [d.put(i) for i in range (1, MAX+1)]
 
-    threads = [mp.Process(target=worker, args=(d, i, sumWorkers)) 
+    threads = [mp.Process(target=worker, args=(lock1, d, i, sumWorkers)) 
         for i in range(0, nthreads)]
     [t.start() for t in threads]
     [t.join() for t in threads]
--- introHPC/09/threading_sync.py
+++ introHPC/09/threading_sync.py
@@ -1,25 +1,27 @@
-from threading import Thread
+from threading import Thread, Lock
 from functools import reduce
 
 MAX = 10000
 nthreads = 2
 
 d = []
-def worker(threadNo, sumWorkers):
+def worker(lock1, threadNo, sumWorkers):
     global d
     sumLocal = 0
     while True:
-        if  len(d) == 0: 
-            break
-        sumLocal += d[0]
-        d = d[1:]
-    sumWorkers[threadNo] = sumLocal
+        with lock1:
+            if  len(d) == 0: 
+                break
+            sumLocal += d[0]
+            d = d[1:]
+        sumWorkers[threadNo] = sumLocal
     print("Thread[%d]: %d" %(threadNo, sumWorkers[threadNo]))
 
 if __name__ == '__main__':
+    lock1 = Lock()
     d = [i for i in range(1, MAX+1)]
     sumWorkers = [0] * nthreads
-    threads = [Thread(target=worker, args=(i, sumWorkers)) 
+    threads = [Thread(target=worker, args=(lock1, i, sumWorkers)) 
         for i in range(0, nthreads)]
     [t.start() for t in threads]
     [t.join() for t in threads]
